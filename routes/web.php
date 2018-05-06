3b8eb0f4dc2872c2cef73f7dd311026079808b9d
Norman Firmansyah Destiawan <norman_fd@apps.ipb.ac.id>
2018-05-06 18:32:09 +0700

    pembaruan routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/post/{post}/edit', 'PostController@edit')->name('post.edit');
 Route::patch('/post/{post}/edit', 'PostController@update')->name('post.update');
 Route::delete('/post/{post}/delete', 'PostController@destroy')->name('post.destroy');
-
 Route::get('/myaccount', 'myaccountController@myaccount')->name('myaccount');
+Route::get('/post/create', 'PostController@create')->name('post.create');
+Route::post('/post/create', 'PostController@store')->name('post.store');
+Route::get('/post', 'PostController@index')->name('post.index');
